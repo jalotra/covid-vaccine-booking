b72c3e4dfa31e8bdb732fdcf72620600a893d182
Shivam <jalotrashivam9@gmail.com>
2021-05-09 19:58:26 +0530

    Just Run Bash Script in src.

---
+++ src/run.sh
@@ -0,0 +1,14 @@
+# If you are doing it for the first time 
+# You will have to input a lot of jargon.
+# Example : Free Vaccine or Not, Refresh Calendar Time, Save as Json or Not
+# So please run this script once to get ahead at real battle time
+
+
+# Change phone number to your registered phone number.
+function first_time(){
+    pip install -r ../requirements.txt
+}
+
+first_time
+
+python covid-vaccine-slot-booking.py --phone 95421XXXXX
